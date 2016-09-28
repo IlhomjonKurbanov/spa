741f5019e0b5aa65b44fdd9338a10964f0f1779f
Huy Nguyen <huynt57@gmail.com>
2016-09-28 11:23:56 +0700

    add migration

---
+++ database/migrations/2016_09_28_110202_create_table_topics.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTopics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('topics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->integer('parent_id')->nullable();
+            $table->integer('template')->nullable();
+            $table->text('data')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('topics');
+    }
+}
